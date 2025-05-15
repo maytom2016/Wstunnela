ed7119bb9690b2a3ea836d0366f6e3546075c67c
maytom2016 <qwerty448qwe@163.com>
2025-05-15 20:45:39 +0800

    upgrade groovy dsl to kotlin dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.feng.wstunnela"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.feng.wstunnela"
+        minSdk = 24
+        targetSdk = 28
+        versionCode = 1
+        versionName = "1.0.6"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+        viewBinding =  true
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.android.basics)
+    implementation(libs.bundles.coroutines)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.bundles.network)
+    implementation(libs.bundles.navigation)
+    implementation(libs.bundles.compose)
+
+    // 单独的依赖项
+    implementation(libs.commons.compress)
+    implementation(libs.commons.io)
+    implementation(libs.legacy.support)
+    implementation(libs.tomlkt)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
